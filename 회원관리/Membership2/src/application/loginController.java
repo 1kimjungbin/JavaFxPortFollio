261c9ed6776d7364a4a2d451145faeff276bea27
1kimjungbin <2020284@sungil-i.kr>
2021-11-04 08:40:35 +0900

    11월 4일 JavaFX 수업

---
+++ 회원관리/Membership2/src/application/loginController.java
@@ -0,0 +1,40 @@
+package application;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class loginController implements Initializable {
+	@FXML TextField txtId;
+	@FXML PasswordField txtPwd;
+	@FXML Button btnLogin, btnReg, btnClose, btnCancel;
+	
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	public void btnLoginAction(ActionEvent event) {
+		
+	}
+	
+	public void btnRegAction(ActionEvent event) {
+		
+	}
+
+	public void btnCloseAction(ActionEvent event) {
+	
+}
+
+	public void btnCancelAction(ActionEvent event) {
+	
+}
+	
+}
---
+++ 회원관리/Membership2/src/module-info.java
@@ -0,0 +1,6 @@
+module kiosk_test {
+	requires javafx.controls;
+	requires javafx.fxml;
+	
+	opens application to javafx.graphics, javafx.fxml;
+}
